df28cc454e398ee3d0671721ed94c463a5a8013f
erarabh <rabhirabah2015@gmail.com>
2025-05-17 15:07:09 +0100

    update supabase

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,17 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Debugging: log environment variables to the console
-console.log("NEXT_PUBLIC_SUPABASE_URL from process.env:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY from process.env:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+console.log("Initializing Supabase...");
+console.log("NEXT_PUBLIC_SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
-// Fetch environment variables
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-												
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Supabase URL or Anonymous Key is missing!");
-}
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log("Supabase client initialized:", supabase);
 
-// Create Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export { supabase };
