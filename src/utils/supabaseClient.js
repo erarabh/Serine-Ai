08da7169a09c7f5a2a0a61f0216b57e20d96292d
erarabh <rabhirabah2015@gmail.com>
2025-05-17 12:25:16 +0100

    update

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,14 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Log environment variables to debug if Vercel reads them
-console.log("NEXT_PUBLIC_SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+// Debugging: log environment variables to the console
+console.log("NEXT_PUBLIC_SUPABASE_URL from process.env:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY from process.env:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 // Fetch environment variables
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Explicit error handling for missing variables
+												
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL or Anonymous Key is missing!");
 }
